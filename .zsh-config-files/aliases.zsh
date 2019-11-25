7da97f8fcba7bd36a0018acc7c3484b5214da8dd
Akshay Nair <phenax5@gmail.com>
2019-11-25 16:47:07 +0530

    Zsh rc update. Refactored config

---
+++ .zsh-config-files/aliases.zsh
@@ -0,0 +1,30 @@
+
+alias open="xdg-open";
+alias show-mobile-screen="adb shell screenrecord --output-format=h264 - | ffplay -";
+
+alias on-complete="notify-send 'Command executed successfully' --urgency normal";
+
+set-php-version() {
+	sudo update-alternatives --set php "/usr/bin/php$1";
+}
+
+set-reminder() {
+	echo "notify-send 'Reminder:: $1'" | at $2
+}
+
+# Monitor aliases
+alias monitor-off="xrandr --output HDMI-1 --off";
+monitor-on() { xrandr --output HDMI-1 --auto --${1:-"right-of"} eDP-1; };
+
+
+# LS aliases 
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
+# Git aliases
+alias gunstage="git reset HEAD --"
+alias gaa="git add ."
+
+
