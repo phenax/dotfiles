b2f775b44e25e5f9c9906e0cca25d0d42aced4c1
Akshay Nair <phenax5@gmail.com>
2019-11-25 16:47:07 +0530

    Zsh rc update. Refactored config

---
+++ .zsh-config-files/paths.zsh
@@ -0,0 +1,32 @@
+
+export GOROOT=/usr/lib/go-1.10
+export GOPATH=/mnt/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.cargo/bin:$HOME/.cabal/bin:/home/linuxbrew/.linuxbrew/bin:$PATH
+
+
+export PATH=$HOME/bash-alias/bin:$PATH;
+
+export FLUTTER_HOME=$HOME/Documents/dev/flutter
+
+export DENO_INSTALL="/home/akshayn/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+export CALIBRE_API_TOKEN=77297a8ca603e9560f05489ba019579e956db3dd587ebf4a03d8e7e501c1355a
+
+# Path to your oh-my-zsh installation.
+export ZSH=/home/akshayn/.oh-my-zsh
+
+
+# Development variables
+export SHAADI=$HOME/dev/shaadi-react;
+export REG_SHAADI=$HOME/dev/shaadi-profile-creation;
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/rsa_id"
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/home/akshayn/.sdkman"
+[[ -s "/home/akshayn/.sdkman/bin/sdkman-init.sh" ]] && source "/home/akshayn/.sdkman/bin/sdkman-init.sh"
+
---
+++ .zsh-config-files/theme.zsh
@@ -0,0 +1,13 @@
+
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+ZSH_THEME="agnoster"
+
+prompt_context() {
+  prompt_segment black default  "🌈 "
+}
+
+prompt_dir() {
+  prompt_segment blue black '%c'
+}
+
+
