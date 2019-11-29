c0a095fa4dbbbb6e0e4f3a922ce8999e2e05d839
Akshay Nair <phenax5@gmail.com>
2019-11-29 08:55:43 +0530

    Updates dotfiles

--- .zsh-config-files/paths.zsh
+++ .zsh-config-files/paths.zsh
@@ -1,23 +1,15 @@
 
 export GOROOT=/usr/lib/go-1.10
 export GOPATH=/mnt/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.cargo/bin:$HOME/.cabal/bin:/home/linuxbrew/.linuxbrew/bin:$PATH
-
-
-export PATH=$HOME/bash-alias/bin:$PATH;
-
-export FLUTTER_HOME=$HOME/Documents/dev/flutter
-
 export DENO_INSTALL="/home/akshayn/.deno"
+
+export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.cargo/bin:$HOME/.cabal/bin:/home/linuxbrew/.linuxbrew/bin:$PATH
+export PATH=$HOME/bash-alias/bin:$PATH;
 export PATH="$DENO_INSTALL/bin:$PATH"
 
 export CALIBRE_API_TOKEN=77297a8ca603e9560f05489ba019579e956db3dd587ebf4a03d8e7e501c1355a
 
-# Path to your oh-my-zsh installation.
-export ZSH=/home/akshayn/.oh-my-zsh
-
-
-# Development variables
+# Development dir variables
 export SHAADI=$HOME/dev/shaadi-react;
 export REG_SHAADI=$HOME/dev/shaadi-profile-creation;
 
