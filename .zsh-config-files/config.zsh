d63c537c2073e940fb1cae8e767b6669695b58e0
Akshay Nair <phenax5@gmail.com>
2019-12-19 10:28:14 +0530

    Updates dotfiles

--- .zsh-config-files/config.zsh
+++
@@ -1,9 +0,0 @@
-export VISUAL='vim'
-export EDITOR="vim"
-export TERMINAL="terminator"
-export BROWSER="google-chrome-stable"
-export READER="zathura"
-export FILE="lf"
-
-export FZF_DEFAULT_OPTS="--layout=reverse --height 40%"
-
