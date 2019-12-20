585ec8f0ca8c3d3699b527bb68b14e8cdcb5b303
Akshay Nair <phenax5@gmail.com>
2019-12-20 08:55:21 +0530

    Updates dotfiles

--- .config/zshconf/vi.zsh
+++ .config/zshconf/vi.zsh
@@ -1,13 +1,13 @@
 
-bindkey -v
+# bindkey -v
 
-export KEYTIMEOUT=1
+# export KEYTIMEOUT=1
 
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '^E' edit-command-line                   # Opens Vim to edit current command line
-bindkey '^R' history-incremental-search-backward # Perform backward search in command line history
-bindkey '^S' history-incremental-search-forward  # Perform forward search in command line history
-bindkey '^P' history-search-backward             # Go back/search in history (autocomplete)
-bindkey '^N' history-search-forward              # Go forward/search in history (autocomplete)
+# autoload -U edit-command-line
+# zle -N edit-command-line
+# bindkey '^E' edit-command-line                   # Opens Vim to edit current command line
+# bindkey '^R' history-incremental-search-backward # Perform backward search in command line history
+# bindkey '^S' history-incremental-search-forward  # Perform forward search in command line history
+# bindkey '^P' history-search-backward             # Go back/search in history (autocomplete)
+# bindkey '^N' history-search-forward              # Go forward/search in history (autocomplete)
 
