575407407628a3d4fb5502c2dbe9207739b8a868
Akshay Nair <phenax5@gmail.com>
2019-12-24 08:11:28 +0530

    Updates dotfiles

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 add_dotfiles() {
-  yadm add ~/.vimrc ~/scripts ~/.scimrc;
+  yadm add ~/.vimrc ~/scripts ~/.scimrc ~/.xinitrc;
   yadm add ~/.Xresources ~/.config/{gtk-3.0,fontconfig,mimeapps.list};
   yadm add ~/.config/{i3,rofi-themes};
   yadm add ~/.config/{terminator,kitty};
