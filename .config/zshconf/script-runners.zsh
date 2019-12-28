5426705ca41e4d5a057bfcc9748abd46af5ec39d
Akshay Nair <phenax5@gmail.com>
2019-12-28 23:32:22 +0530

    Updates dotfiles

--- .config/zshconf/script-runners.zsh
+++ .config/zshconf/script-runners.zsh
@@ -1,8 +1,23 @@
 
-_create_runner() { alias $1="~/scripts/${2:-"$1.sh"}"; }
+BIN_DIR="$SCRIPTS_BINARY_DIR";
+SCRIPT="$SCRIPTS_DIR";
+
+_create_runner() {
+  local name="$1";
+  local filename="${2:-"$1.sh"}";
+  local filepath="$SCRIPT/$filename";
+
+  alias $name="$filepath";
+
+  # Create a binary file with symlink to script
+  # if [ ! -f "$BIN_DIR/$name" ]; then
+    # ln -s "$filepath" "$BIN_DIR/$name";
+  # fi;
+}
 
 _create_runner "image-preview";
 _create_runner "brightness";
 _create_runner "lockscreen";
+_create_runner "open-project";
 _create_runner "xres-template" "xconfig.js";
 
