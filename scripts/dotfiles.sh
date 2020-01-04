744deb12d570a7addc5221f4c8ba441a96780e98
Akshay Nair <phenax5@gmail.com>
2020-01-04 19:59:41 +0530

    Updates dotfiles

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+add() { yadm add "$@" || true; }
+
 # X
-yadm add ~/.config/{i3,rofi-themes};
-yadm add ~/.Xresources ~/.config/{gtk-3.0,fontconfig,mimeapps.list};
+add ~/.config/{i3,rofi-themes};
+add ~/.Xresources ~/.config/{gtk-3.0,fontconfig,mimeapps.list};
 
 # Terminal
-yadm add ~/.bashrc ~/.zshrc ~/.config/zshconf ~/.profile;
-yadm add ~/.config/{terminator,kitty};
+add ~/.bashrc ~/.zshrc ~/.config/zshconf ~/.profile;
+add ~/.config/{terminator,kitty};
 
 # Dev
-yadm add ~/.vimrc ~/scripts ~/.scimrc ~/.xinitrc ~/.tmux.conf ~/.gitconfig ~/.yarnrc ~/.npmrc;
+add ~/.vimrc ~/scripts ~/.scimrc ~/.xinitrc ~/.tmux.conf ~/.gitconfig ~/.yarnrc ~/.npmrc;
 
 # Applications
-yadm add ~/.config/{lf,calcurse,clipit,org,pet,qutebrowser};
-yadm add ~/.fehbg;
+add ~/.config/{lf,calcurse,org,pet,qutebrowser};
+add ~/.fehbg;
 
