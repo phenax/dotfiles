066b731b8427b7d6f1f3196c639488a8850356bc
Akshay Nair <phenax5@gmail.com>
2020-01-05 19:16:54 +0530

    Updates dotfiles

--- scripts/bookmarks-menu/bookmarker.sh
+++ scripts/bookmarks-menu/bookmarker.sh
@@ -2,17 +2,24 @@
 
 # TODO: Convert bookmarks to json array and use jq
 
+source ~/scripts/modules/rofi-menu.sh;
 source ~/scripts/modules/utils.sh;
 
 BOOKMARKER_FILE="$HOME/scripts/bookmarks-menu/bookmarks";
 
 get_column() { awk "BEGIN {FS=\"|\"}; $1"; }
 
+OPEN="⚟ Open";
+COPY="⎘ Copy";
+TYPE="⌨ Type";
+DELETE="✗ Delete";
+ADD="add";
+
 options=$(echo "
-open
-copy
-type
-delete
+$OPEN
+$COPY
+$TYPE
+$DELETE
 " | trim)
 
 get_options() { echo "$options"; }
@@ -31,8 +38,8 @@ get_title() { get_column '{ print $1 }'; }
 get_url() { get_column '{ print $2 }'; }
 find_by_title() { cat $BOOKMARKER_FILE | sed "/^$1|/!d" | head -n 1; }
 
-display_bookmarks() { get_bookmarks | get_title | rofi -dmenu -p "Bookmarks"; }
-display_options() { get_options | rofi -dmenu -p "Action"; }
+display_bookmarks() { get_bookmarks | get_title | open-menu -p "Bookmarks"; }
+display_options() { get_options | open-block-menu -p "Action"; }
 
 move_bookmark_up() {
   bookmark=$(find_by_title "$1");
@@ -49,18 +56,18 @@ menu() {
   action="print";
 
   if [[ -z "$bookmark_url" ]]; then
-    action="add";
+    action=$ADD;
   else
     action=$(display_options);
   fi
 
   case "$action" in
-    copy) echo "$bookmark_url" | copy && move_bookmark_up "$selected_title" ;;
-    open) xdg-open "$bookmark_url" && move_bookmark_up "$selected_title" ;;
-    delete) delete_bookmark $(find_by_title "$selected_title") ;;
-    type) xdotool type --delay 30 "$bookmark_url" ;;
-    add)
-      url=$(rofi -dmenu -p "URL for $selected_title");
+    $COPY) echo "$bookmark_url" | copy && move_bookmark_up "$selected_title" ;;
+    $OPEN) xdg-open "$bookmark_url" && move_bookmark_up "$selected_title" ;;
+    $DELETE) delete_bookmark $(find_by_title "$selected_title") ;;
+    $TYPE) xdotool type --delay 30 "$bookmark_url" ;;
+    $ADD)
+      url=$(open-menu -p "URL for $selected_title");
       [[ -z "$url" ]] && exit 0;
       add_bookmark "$selected_title" "$url";
     ;;
