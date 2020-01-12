1fd878b0076600fb316329e380f43ab20b341a79
Akshay Nair <phenax5@gmail.com>
2020-01-12 17:12:11 +0530

    Updates dotfiles

--- scripts/menuitems.sh
+++ scripts/menuitems.sh
@@ -2,10 +2,16 @@
 
 battery() { echo -e "$(cat "/sys/class/power_supply/BAT0/$1")"; }
 
+get_volume() {
+  amixer get Master |
+    awk -F'[][]' '/dB/ {print $6 " " $2}' |
+    sed 's/on //; s/off/muted/';
+}
+
 case "$1" in
   brightness) echo "$(echo "($(~/scripts/brightness.sh get) * 100)/1" | bc)" ;;
   battery) echo "$(battery status) $(battery capacity)" ;;
-  volume) echo "$(amixer get Master | grep % | awk '{print $4}' | sed 's/[^0-9\%]//g' | head -n1)" ;;
+  volume) get_volume ;;
   do-show-time) kitty -e "calcurse" ;;
   *) echo "Not a valid menu item" ;;
 esac
