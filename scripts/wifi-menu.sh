5f0be61cf896810498f15add3311092d31112384
Akshay Nair <phenax5@gmail.com>
2020-01-12 16:48:55 +0530

    Updates dotfiles

---
+++ scripts/wifi-menu.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+source $HOME/scripts/modules/utils.sh;
+source $HOME/scripts/modules/rofi-menu.sh;
+
+ACTIVE="◼"
+INACTIVE="◻"
+
+# :: (on|off) -> string -> string -> string
+ifOnElse() { [[ "$1" == "on" ]] && echo "$2" || echo "$3"; }
+# :: (on|off) -> (on|off)
+inverse() { ifOnElse "$1" "off" "on"; }
+
+get_options() {
+  local state="$1";
+
+  echo "
+$(ifOnElse "$state" $ACTIVE $INACTIVE)  Turn wifi $(inverse $state)
+$([[ $1 == "on" ]] && echo "☰  Select wifi network")
+" | trim;
+}
+
+# :: string -> Bool
+is_known_connection() {
+  local ssid=$1;
+  local match=$(nmcli -t connection show | awk -F: '$1 == ssid {print $0}' ssid="$ssid");
+  [[ "$match" != "" ]];
+}
+
+# :: () -> (on|off)
+get_wifi_status() {
+  nmcli -f wifi general status | tail -n 1 | sed 's/enabled/on/; s/disabled/off/;';
+}
+# :: (on|off) -> ()
+set_wifi_status() { nmcli radio wifi $1; }
+
+get_wifi_networks() {
+  nmcli -f active,ssid,rate,bars,security dev wifi | tail -n +2 |
+    awk '{sub(/^yes/,   ACTIVE " "  )}1' ACTIVE="$ACTIVE" |
+    awk '{sub(/^no/,    INACTIVE    )}1' INACTIVE="$INACTIVE";
+}
+
+# :: string -> string
+get_ssid() { awk '{ print $2 }'; }
+
+
+show_wifi_networks_menu() {
+  local wifi_ssid=$(get_wifi_networks | open-menu -p "Wifi Networks" | get_ssid);
+  [[ -z "$wifi_ssid" ]] && return 1;
+
+  if $(is_known_connection "$wifi_ssid"); then
+    nmcli con up "$wifi_ssid";
+  else
+    # TODO: Connect to new network
+    echo "TODO: Implement connecting to new networks";
+  fi;
+}
+
+show_menu() {
+  local state=$(get_wifi_status);
+  local options=$(get_options $state);
+  local action=$(echo "$options" | open-menu -p "Wifi Menu");
+
+  [[ -z "$action" ]] && return 1;
+
+  case "$action" in
+    *Turn\ wifi*)    set_wifi_status $(inverse $state) ;;
+    *Select\ wifi*)  show_wifi_networks_menu ;;
+    *) ;;
+  esac
+}
+
+case $1 in
+  menu) show_menu ;;
+  wifi_menu) show_wifi_networks_menu ;;
+  list) get_wifi_networks ;;
+  on) set_wifi_status on ;;
+  off) set_wifi_status off ;;
+  *) echo "Usage:: wifi-menu (menu|on|off)" ;;
+esac
