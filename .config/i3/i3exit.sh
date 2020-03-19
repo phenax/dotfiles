0af1cdd6433ad36128465eddd7c35392c27e013c
Akshay Nair <phenax5@gmail.com>
2020-03-19 13:29:56 +0530

    Updates dotfiles

--- .config/i3/i3exit.sh
+++ .config/i3/i3exit.sh
@@ -19,10 +19,10 @@ case "$1" in
         systemctl hibernate
         ;;
     reboot)
-        systemctl reboot
+        bspc quit && systemctl reboot;
         ;;
     shutdown)
-        systemctl poweroff
+        bspc quit && systemctl poweroff;
         ;;
     *)
         echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
