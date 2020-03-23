8d8664087fdfe39b86990506aa825b93bd7bb556
Akshay Nair <phenax5@gmail.com>
2020-03-23 21:18:53 +0530

    Updates dotfiles

--- scripts/sessions/project.sh
+++ scripts/sessions/project.sh
@@ -5,15 +5,14 @@ workspace="$2";
 
 [[ ! -z "$workspace" ]] && bspc desktop -f "$workspace";
 
-$TERMINAL --title "Code Editor" -d "$dir" i3-sensible-editor . &
-sleep 0.2;
-$TERMINAL -d "$dir" &
+$TERMINAL -d "$dir" -e i3-sensible-editor . &
+sleep 0.3;
 $TERMINAL -d "$dir" &
 $TERMINAL -d "$dir" &
 
 sleep 0.5;
 
-~/.config/bspwm/layouts/tall.sh;
+bspc node '@/1.!hidden' -f;
 
 disown;
 
