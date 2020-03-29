5287f03254d9749f71cdaa3fdb7212bcdc108aff
Akshay Nair <phenax5@gmail.com>
2020-03-29 23:29:50 +0530

    Updates dotfiles

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# Clear the last preview (if any)
-$HOME/.config/lf/image clear
-
-# Calculate where the image should be placed on the screen.
-num=$(printf "%0.f\n" "`echo "$(tput cols) / 2" | bc`")
-numb=$(printf "%0.f\n" "`echo "$(tput cols) - $num - 1" | bc`")
-numc=$(printf "%0.f\n" "`echo "$(tput lines) - 2" | bc`")
-
 case "$1" in
+  # Archives
 	*.tgz|*.tar.gz) tar tzf "$1";;
 	*.tar.bz2|*.tbz2) tar tjf "$1";;
 	*.tar.txz|*.txz) xz --list "$1";;
@@ -16,39 +9,28 @@ case "$1" in
 	*.zip|*.jar|*.war|*.ear|*.oxt) unzip -l "$1";;
 	*.rar) unrar l "$1";;
 	*.7z) 7z l "$1";;
-	*.[1-8]) man "$1" | col -b ;;
 	*.o) nm "$1" | less ;;
-	# *.torrent) transmission-show "$1";;
-	# *.iso) iso-info --no-header -l "$1";;
-	# *odt,*.ods,*.odp,*.sxw) odt2txt "$1";;
-	# *.doc) catdoc "$1" ;;
-	# *.docx) docx2txt "$1" - ;;
+
+	# Docs
+  *.md|*.org) glow -s dark "$1" ;;
 	*.csv) cat "$1" | sed s/,/\\n/g ;;
-	*.pdf)
-	  echo "[Loading...]";
-		CACHE=$(mktemp /tmp/thumbcache.XXXXX)
-		pdftoppm -png -f 1 -singlefile "$1" "$CACHE"
-		$HOME/.config/lf/image draw "$CACHE.png" $num 1 $numb $numc
+	*.pdf) pdftotext "$1" ;;
+	*.epub) mediainfo "$1" ;;
+	*.[1-8]) man "$1" | col -b ;;
+
+	# Images
+	*.bmp|*.jpg|*.jpeg|*.png|*.xpm|*.webp) mediainfo "$1"; ;;
+
+	# Audio
+  *.wav|*.mp3|*.flac|*.m4a|*.wma|*.ape|*.ac3|*.og[agx]|*.spx|*.opus|*.as[fx]|*.flac)
+    mediainfo "$1"
   ;;
-	*.epub)
-	  echo "[Loading...]";
-		CACHE=$(mktemp /tmp/thumbcache.XXXXX)
-		epub-thumbnailer "$1" "$CACHE" 1024
-		$HOME/.config/lf/image draw "$CACHE" $num 1 $numb $numc
+
+  # Video
+  *.avi|*.mp4|*.wmv|*.dat|*.3gp|*.ogv|*.mkv|*.mpg|*.mpeg|*.vob|*.fl[icv]|*.m2v|*.mov|*.webm|*.ts|*.mts|*.m4v|*.r[am]|*.qt|*.divx)
+    mediainfo "$1";
   ;;
-	*.bmp|*.jpg|*.jpeg|*.png|*.xpm)
-	  echo "[Loading...]";
-		$HOME/.config/lf/image draw "$1" $num 1 $numb $numc
-  ;;
-	# *.wav|*.mp3|*.flac|*.m4a|*.wma|*.ape|*.ac3|*.og[agx]|*.spx|*.opus|*.as[fx]|*.flac)
-		# exiftool "$1"
-	# ;;
-	# *.avi|*.mp4|*.wmv|*.dat|*.3gp|*.ogv|*.mkv|*.mpg|*.mpeg|*.vob|*.fl[icv]|*.m2v|*.mov|*.webm|*.ts|*.mts|*.m4v|*.r[am]|*.qt|*.divx)
-		# CACHE=$(mktemp /tmp/thumbcache.XXXXX)
-		# ffmpegthumbnailer -i "$1" -o "$CACHE" -s 0
-		# $HOME/.config/lf/image draw "$CACHE" $num 1 $numb $numc
-  # ;;
-	*)
-	  highlight --out-format ansi "$1" || cat "$1"
-	;;
+
+  # Syntax
+	*) highlight --out-format ansi "$1" || cat "$1" ;;
 esac
