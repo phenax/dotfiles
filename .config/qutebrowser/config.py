de2b0e6c5b01fcc89b557ffe57fa71414c689420
Akshay Nair <phenax5@gmail.com>
2020-05-20 20:30:47 +0530

    Updates dotfiles

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,12 +3,24 @@ import os
 import re
 import sys
 import json
+import subprocess
 from qutebrowser.config.configfiles import ConfigAPI
 from qutebrowser.config.config import ConfigContainer
 config = config
 c = c
 
 # Helper Functions {{{
+def read_xresources(prefix):
+    props = {}
+    x = subprocess.run(['xrdb', '-query'], stdout=subprocess.PIPE)
+    lines = x.stdout.decode().split('\n')
+    for line in filter(lambda l : l.startswith(prefix), lines):
+        prop, _, value = line.partition(':\t')
+        props[prop] = value
+    return props
+
+xresources = read_xresources('*')
+
 def bind(key, command, mode):  # noqa: E302
     """Bind key to command in mode."""
     config.bind(key, command, mode=mode)
@@ -125,37 +137,38 @@ c.colors.hints.bg = 'yellow'
 c.hints.border = '1px solid #000000'
 
 
+#  c.colors.statusbar.normal.bg = xresources['*.background']
 ## Context menu styles
-c.colors.contextmenu.menu.bg = "#0f1215"
-c.colors.contextmenu.menu.fg = "white"
-c.colors.contextmenu.selected.bg = "#333333"
-c.colors.contextmenu.selected.fg = "white"
+c.colors.contextmenu.menu.bg = xresources['*.background']
+c.colors.contextmenu.menu.fg = xresources['*.foreground']
+c.colors.contextmenu.selected.bg = xresources['*.color8']
+c.colors.contextmenu.selected.fg = xresources['*.foreground']
 
 ## Completion
-c.colors.completion.category.bg = "#000000"
-c.colors.completion.even.bg = "#0f1215"
-c.colors.completion.odd.bg = "#1f2225"
-c.colors.completion.item.selected.bg = "#8ec07c"
+c.colors.completion.category.bg = xresources['*.background']
+c.colors.completion.even.bg = xresources['*.background']
+c.colors.completion.odd.bg = xresources['*.color0']
+c.colors.completion.item.selected.bg = xresources['*.color14']
 
 ## Tabs
 c.tabs.padding = { 'bottom': 2, 'top': 2, 'left': 5, 'right': 5 }
 
 ## Unselected tabs
-c.colors.tabs.even.bg = "#0f1215"
-c.colors.tabs.even.fg = "white"
+c.colors.tabs.even.bg = xresources['*.background']
+c.colors.tabs.even.fg = xresources['*.foreground']
 c.colors.tabs.odd.bg = c.colors.tabs.even.bg
 c.colors.tabs.odd.fg = c.colors.tabs.even.fg
 
 ## Selected tab
-c.colors.tabs.selected.even.bg = '#505C95'
-c.colors.tabs.selected.even.fg = 'white'
+c.colors.tabs.selected.even.bg = '#505C95' # xresources['*.color4']
+c.colors.tabs.selected.even.fg = xresources['*.foreground']
 c.colors.tabs.selected.odd.bg = c.colors.tabs.selected.even.bg
 c.colors.tabs.selected.odd.fg = c.colors.tabs.selected.even.fg
 
 # Pinned tab
-c.colors.tabs.pinned.even.bg = '#1f2225'
+c.colors.tabs.pinned.even.bg = xresources['*.background']
 c.colors.tabs.pinned.even.fg = c.colors.tabs.even.fg
-c.colors.tabs.pinned.odd.bg = '#1f2225'
+c.colors.tabs.pinned.odd.bg = xresources['*.background']
 c.colors.tabs.pinned.odd.fg = c.colors.tabs.odd.fg
 
 c.colors.tabs.pinned.selected.even.bg = c.colors.tabs.selected.even.bg
@@ -244,9 +257,8 @@ c.content.host_blocking.enabled = True
 c.content.media_capture = 'ask'
 c.content.notifications = 'ask'
 c.content.ssl_strict = 'ask'
-c.content.desktop_capture = False
-c.content.mouse_lock = False
-c.content.xss_auditing = True
+c.content.desktop_capture = 'ask'
+c.content.mouse_lock = 'ask'
 # c.content.canvas_reading = False
 # }}}
 
@@ -271,7 +283,7 @@ c.aliases['archive'] = 'open --tab http://web.archive.org/save/{url}'
 c.aliases['view-archive'] = 'open --tab http://web.archive.org/web/*/{url}'
 c.aliases['view-google-cache'] = 'open --tab http://www.google.com/search?q=cache:{url}'
 
-c.aliases['xa'] = 'quit --save'
+#  c.aliases['xa'] = 'quit --save'
 c.aliases['h'] = 'help'
 # }}}
 
@@ -280,7 +292,9 @@ nmap('\\tr', 'config-source')    # Reload config
 nmap('\\ti', 'inspector')        # Inspector
 nmap('\\ts', 'view-source')      # View page source
 
-nmap('\\tj', 'spawn --userscript format_json') # Json formatter
+# Json formatter
+c.aliases['format-json'] = 'spawn --userscript format_json';
+nmap('\\tj', 'format-json')
 # }}}
 
 #### History {{{
--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -1,69 +1,23 @@
 #!/bin/bash
 
-# https://community.spotify.com/t5/Desktop-Linux/Basic-controls-via-command-line/td-p/4295625
-
 spotify_launch() { spotify; }
 
-SP_DEST=org.mpris.MediaPlayer2.spotify;
-SP_PATH=/org/mpris/MediaPlayer2;
-SP_MEMB=org.mpris.MediaPlayer2.Player;
-
-# Extract value of a property from dictionary or array
-extract() { echo "$1" | awk -F'|' '$1 == MATCH { print $2 }' MATCH="$2"; }
-
-# Sanitize output string
-# sanitize() { sed 's/[^A-Za-z0-9\-\(\)]/ /g' | sed 's/\s\+/ /g'; }
-sanitize() { sed 's/\s\+/ /g'; }
-
-# Get a property from media player interface
-spotify_get_property() {
-  dbus-send                                                                   \
-  --print-reply                                         \
-  --dest=$SP_DEST                                                             \
-  $SP_PATH                                                                    \
-  org.freedesktop.DBus.Properties.Get                                         \
-  string:"$SP_MEMB" string:$1                                                 \
-    | grep -Ev "^method"                              \
-    | grep -Eo '("(.*)")|(\b[0-9][a-zA-Z0-9.]*\b)' \
-    | sed -E '2~2 a|'                                       \
-    | tr -d '\n'                                        \
-    | sed -E 's/\|/\n/g'                                   \
-    | sed -E 's/(xesam:)|(mpris:)//'                     \
-    | sed -E 's/^"//'                                       \
-    | sed -E 's/"$//'                                \
-    | sed -E 's/"+/|/'                               \
-    | sed -E 's/ +/ /g'                            
-}
-
-# Send a signal to player
-spotify_signal() {
-  dbus-send --print-reply --dest=$SP_DEST $SP_PATH org.mpris.MediaPlayer2.Player.$1;
-}
-
-# Get song metadata
-get_metadata() { spotify_get_property Metadata; }
-
 # Get player state
-get_play_state() { spotify_get_property PlaybackStatus; }
+get_play_state() { playerctl metadata --format '{{status}}'; }
 
 # Get title - artist (song label)
-get_label() {
-  local meta=$(get_metadata);
-  local artist=$(extract "$meta" artist | sanitize);
-  local title=$(extract "$meta" title | sanitize);
-  local label="$title - $artist";
-  echo "$label";
-}
+get_label() { playerctl metadata --format '{{title}} - {{artist}}'; }
 
 # Play/Pause toggle:
-play_pause() { spotify_signal PlayPause; }
+play_pause() { playerctl play-pause; }
 
 # Next/Prev
-next() { spotify_signal Next; }
-prev() { spotify_signal Previous; }
+next() { playerctl next; }
+prev() { playerctl previous; }
 
 notify() {
-  notify-send "Song change $1 $2 $3";
+  echo "1";
+  # notify-send "Playing $(get_label)...";
 }
 
 case "$1" in
