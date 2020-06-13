67fd52842bee0079a2327fbbfec8b3ea0682a285
Akshay Nair <phenax5@gmail.com>
2020-06-13 22:11:37 +0530

    Updates dotfiles

--- .config/zshconf/aliases/dir.zsh
+++ .config/zshconf/aliases/dir.zsh
@@ -1,8 +1,23 @@
+take() { mkdir -p $@ && cd ${@:$#}; }
 
-take() {
-  mkdir -p $@ && cd ${@:$#}
-}
+alias ..='cd ..';
+alias ...='cd ../..';
 
-alias ..="cd ..";
-alias ...="cd ../..";
+alias gdv='cd ~/dev/';
+
+alias gpic='cd ~/Pictures';
+alias gdl='cd ~/Downloads/dl';
+alias gm='cd ~/Downloads/music';
+
+alias gnotes='cd ~/.config/vimwiki';
+
+alias gvim='cd ~/.config/nvim';
+alias gcmd='cd ~/scripts/commands';
+alias gzsh='cd ~/.config/zshconf';
+
+alias gsuck='cd ~/.config/suckless';
+alias gdwm='cd ~/.config/suckless/dwm';
+alias gdwmb='cd ~/.config/suckless/dwmbar';
+
+alias gkey='cd ~/.config/sxhkd';
 
