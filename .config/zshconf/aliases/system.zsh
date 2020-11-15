fb552eed116e9a1ead98cddf21afb6cda23c516e
Akshay Nair <phenax5@gmail.com>
2020-11-15 19:36:45 +0530

    Updates dotfiles

--- .config/zshconf/aliases/system.zsh
+++ .config/zshconf/aliases/system.zsh
@@ -7,3 +7,7 @@ alias mirrorlist-refresh="sudo reflector --latest 200 --protocol http --protocol
 # Sandbox
 alias sandbox="sudo ~/scripts/host-file-elb-update.sh";
 
+# Runit helpers
+sv_enable() { sudo ln -s /etc/runit/sv/$1 /run/runit/service; }
+sv_disable() { sudo unlink /run/runit/service/$1; }
+
