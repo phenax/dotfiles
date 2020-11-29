4fb746ab6d744249793baa0379d0210dfe77ded7
Akshay Nair <phenax5@gmail.com>
2020-11-30 00:03:46 +0530

    Updates dotfiles

--- .config/zshconf/aliases/dev.zsh
+++ .config/zshconf/aliases/dev.zsh
@@ -1,3 +1,10 @@
 
 alias aws="docker run --rm -it amazon/aws-cli"
 
+far() {
+  local file_r="$1"; shift;
+  local matcher_r="$1"; shift;
+  local result="$1"; shift;
+  fd "$file_r" | sad "$matcher_r" "$result" -p diff-so-fancy "$@";
+}
+
